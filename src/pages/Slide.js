d60b754e393369db4fb35c8f602a97230bd9d5d6
saimang12@naver.com <saimang12@naver.com>
2023-04-06 09:16:23 +0900

    2023-04-06

--- src/pages/Slide.js
+++ src/pages/Slide.js
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom";
 import MainSlide from "react-slick";
 import "slick-carousel/slick/slick.css";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import { useRef } from "react";
 
 const Slide = ({ busanFood }) => {
+
+    const s = useRef(null)
+
     const MainSlideOption = {
         arrows: false,
         slidesToShow: 3,
@@ -11,7 +16,7 @@ const Slide = ({ busanFood }) => {
     return (
         <div className="MainVisual">
             <div className="inner">
-                <MainSlide {...MainSlideOption}>
+                <MainSlide {...MainSlideOption} ref={s}>
                     {
                         busanFood &&
                         busanFood.map((it, idx) => {
@@ -38,6 +43,10 @@ const Slide = ({ busanFood }) => {
 
                     }
                 </MainSlide>
+                <div className="arrows">
+                    <BsChevronLeft onClick={() => { s.current.slickPrev() }} className="left" />
+                    <BsChevronRight onClick={() => { s.current.slickNext() }} className="right" />
+                </div>
             </div>
         </div>
     )
